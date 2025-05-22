95420056b46b750c2004b7e539e69187a3ea6bf3
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-05-22 12:23:51 +0300

    order.py completed  :)

--- order.py
+++ order.py
@@ -2,4 +2,27 @@ class Order:
 
     all_orders = []
 
+    def __init__(self, customer, coffee, price):
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
+
+        Order.all_orders.append(self)
+
+        if not (1.0 <= price <= 10.0):
+            raise TypeError("Price must be within the correct range!!")
+
+        if not isinstance(price, float):
+            raise TypeError("The price must be a float!!")
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property 
+    def price(self):
+        return self._price
     
+    @property 
+    def coffee(self):
+        return self._coffee
